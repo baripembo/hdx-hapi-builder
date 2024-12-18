05b68192936ae81c5178b25404836646f52cafc2
baripembo <erikawei@gmail.com>
2024-12-18 12:46:13 -0500

    fixing base path

--- src/main.js
+++ src/main.js
@@ -1,16 +1,3 @@
-// import { mount } from 'svelte'
-// import './app.css'
-// import App from './App.svelte'
-
-// const app = mount(App, {
-//   target: document.getElementById('app'),
-// })
-
-// export default app
-
-
-
-// src/main.js
 import { mount } from 'svelte'
 import './app.scss'
 import App from "./App.svelte";
@@ -18,11 +5,6 @@ import App from "./App.svelte";
 const urlParams = new URLSearchParams(window.location.search);
 const pageType = urlParams.get("type") || "locations";
 const params = Object.fromEntries(urlParams.entries());
-
-// const app = new App({
-//   target: document.body,
-//   props: { pageType, params },
-// });
 
 const app = mount(App, {
   target: document.getElementById('app'),
